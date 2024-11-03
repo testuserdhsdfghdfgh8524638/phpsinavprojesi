7ac920976ef3b7a89ef709f5ca28b478d276ad21
testuserdhsdfghdfgh8524638 <testregister509@gmail.com>
2024-11-04 01:32:27 +0300

    Update alt.php

--- yonet - Kopya/alt.php
+++ yonet - Kopya/alt.php
@@ -1,95 +1,58 @@
-  <!-- Control Sidebar -->
-  <aside class="control-sidebar control-sidebar-dark">
-    <!-- Control sidebar content goes here -->
-    <div class="p-3">
-      <h5>Title</h5>
-      <p>Sidebar content</p>
-    </div>
-  </aside>
-  <!-- /.control-sidebar -->
+<?php
+// Eğer şu anki sayfa 'urun_detay.php' ise
+if (strpos($_SERVER['SCRIPT_NAME'], 'urun_detay.php')) {
+    // Summernote eklentisini dahil et ve açıklama alanını başlat
+    echo '<script src="plugins/summernote/summernote-bs4.min.js"></script>
+    <script>
+      $(function () {
+        // 'urun_aciklama' alanı için Summernote editorünü başlat
+        $("#urun_aciklama").summernote({height: 250});
+      });
+    </script>';
+}
 
-  <!-- Main Footer -->
-  <footer class="main-footer">
-    <!-- To the right -->
-    <div class="float-right d-none d-sm-inline">
-      Anything you want
-    </div>
-    <!-- Default to the left -->
-    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
-  </footer>
-  </div>
-  <!-- ./wrapper -->
-
-  <!-- REQUIRED SCRIPTS -->
-
-  <!-- jQuery -->
-  <script src="plugins/jquery/jquery.min.js"></script>
-  <!-- SweetAlert2 -->
-  <script src="plugins/sweetalert2/sweetalert2.min.js"></script>
-  <!-- Bootstrap 4 -->
-  <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <!-- DataTables  & Plugins -->
-  <script src="plugins/datatables/jquery.dataTables.min.js"></script>
-  <script src="plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
-  <script src="plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
-  <script src="plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
-  <script src="plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
-  <script src="plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
-  <script src="plugins/jszip/jszip.min.js"></script>
-  <script src="plugins/pdfmake/pdfmake.min.js"></script>
-  <script src="plugins/pdfmake/vfs_fonts.js"></script>
-  <script src="plugins/datatables-buttons/js/buttons.html5.min.js"></script>
-  <script src="plugins/datatables-buttons/js/buttons.print.min.js"></script>
-  <script src="plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
-  <!-- AdminLTE App -->
-  <script src="dist/js/adminlte.min.js"></script>
-
-  <?php
-if(strpos($_SERVER['SCRIPT_NAME'],'urun_detay.php'))
-  echo '<script src="plugins/summernote/summernote-bs4.min.js"></script>
-  <script>$(function () {$("#urun_aciklama").summernote({height: 250})})</script>';
-
-  if (isset($_GET['sonuc'])) {
-  ?>
+// 'sonuc' GET parametresi varsa
+if (isset($_GET['sonuc'])) {
+?>
     <script>
       $(function() {
+        // SweetAlert2 için Toast ayarlarını oluştur
         var Toast = Swal.mixin({
           toast: true,
-          position: 'top-end',
-          showConfirmButton: false,
-          timer: 3000
+          position: 'top-end', // Toast pozisyonu
+          showConfirmButton: false, // Onay butonunu gizle
+          timer: 3000 // 3 saniye sonra kapanacak
         });
         Toast.fire({
           <?php
+          // 'sonuc' değeri başarılı mı?
           if ($_GET['sonuc']) {
           ?>
-            icon: 'success',
-            title: 'Kayıt İşlemi Başarılı'
+            icon: 'success', // Başarılı ikon
+            title: 'Kayıt İşlemi Başarılı' // Başarılı mesajı
           <?php
           } else {
           ?>
-            icon: 'error',
-            title: 'Hata Alındı Tekrar Deneyiniz'
+            icon: 'error', // Hata ikon
+            title: 'Hata Alındı Tekrar Deneyiniz' // Hata mesajı
           <?php
           }
           ?>
-        })
-      })
+        });
+      });
     </script>
-  <?php
-  }
-  ?>
+<?php
+}
+?>
 
-  <script>
-    $(function() {
-      $("#example1").DataTable({
-        "responsive": true,
-        "lengthChange": false,
-        "autoWidth": false,
-        "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
-      }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
-    })
-  </script>
-  </body>
-
-  </html>
+<script>
+  $(function() {
+    // DataTable'ı başlat ve ayarlarını yapılandır
+    $("#example1").DataTable({
+      "responsive": true, // Responsive yapı
+      "lengthChange": false, // Uzunluk değiştirme özelliğini kapat
+      "autoWidth": false, // Otomatik genişliği kapat
+      "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"] // Kullanılacak butonlar
+    }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)'); // Butonları belirli bir yere yerleştir
+  });
+</script>
